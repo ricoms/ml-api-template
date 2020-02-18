8c35c701ebda12484c4d91ee478b111bef812a32
ricoms <ricardosavii@gmail.com>
2020-02-18 02:02:06 -0300

    [fix] organize overall modules

---
+++ src/utils/arg_parser.py
@@ -0,0 +1,119 @@
+import argparse
+import configparser
+import datetime
+import multiprocessing
+import os
+import time
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Any, Dict
+
+class ArgParser(ABC):
+
+    def __init__(self):
+        environment = os.environ.get("ENVIRON", "LOCAL")
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        config = config[environment]
+
+        self.proj_prefix = Path(config.get("PROJ_PREFIX", "/opt/program"))
+        self.ml_prefix = Path(config.get("ML_PREFIX", "/opt/ml"))
+        self.output_prefix = self.ml_prefix / 'output'
+        self.data_prefix = self.ml_prefix / 'input/data/'
+        self.config_prefix = self.ml_prefix / 'input/config'
+        self.aws_param_file = self.ml_prefix / "input/config/hyperparameters.json"
+        self.run_tag = datetime.datetime \
+            .fromtimestamp(time.time()) \
+            .strftime('%Y-%m-%d-%H-%M-%S')
+
+    @abstractmethod
+    def get_arguments(self) -> Dict[str, Any]:
+        pass
+
+
+class LocalArgParser(ArgParser):
+
+    def get_arguments(self) -> Dict[str, Any]:
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            '--data_path',
+            default=self.data_prefix / 'training',
+            type=Path,
+            help=f"Path to a local storage (default: \
+                '{str(self.data_prefix / 'training')}')",
+        )
+        parser.add_argument(
+            '--output_base_path',
+            default=str(self.output_prefix),
+            type=Path,
+            help=f"Path to a local storage (default: \
+                '{str(self.output_prefix)}')",
+        )
+        parser.add_argument(
+            '--project_name',
+            default=self.run_tag,
+            type=str,
+            help=f"Run ID (default: '{self.run_tag}')",
+        )
+        parser.add_argument(
+            '--run_tag',
+            default=self.run_tag,
+            type=str,
+            help=f"Run ID (default: '{self.run_tag}')",
+        )
+        parser.add_argument(
+            '--bucket_name',
+            type=str,
+            default='',
+            help="Bucket name of a remote storage (default: '')"
+        )
+        args = parser.parse_args()
+
+        return args
+
+
+class APIArgParser(ArgParser):
+
+    def get_arguments(self) -> Dict[str, Any]:
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            '--model_base_path',
+            default=self.ml_prefix,
+            type=Path,
+            help=f"Path to a local storage (default: \
+                '{str(self.ml_prefix)}')",
+        )
+        parser.add_argument(
+            '--bucket_name',
+            type=str,
+            default='',
+            help="Bucket name of a remote storage (default: '')"
+        )
+        parser.add_argument(
+            '--project_name',
+            default='teste',
+            type=str,
+            help=f"Project name",
+        )
+        parser.add_argument(
+            '--run_tag',
+            default=self.run_tag,
+            type=str,
+            help=f"Run ID (default: '{self.run_tag}')",
+        )
+        cpu_count = multiprocessing.cpu_count()
+        parser.add_argument(
+            '--model_server_workers',
+            default=cpu_count,
+            type=Path,
+            help=f" Number of model server workers (default: '{cpu_count}')",
+        )
+        parser.add_argument(
+            '--model_server_timeout',
+            default=60,
+            type=Path,
+            help=f"Number of model server workers (default: 60)",
+        )
+        args = parser.parse_args()
+
+        return args
