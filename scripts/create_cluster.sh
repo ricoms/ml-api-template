2042888a74af7586218159b611501f8af5bbddeb
ricoms <ricardosavii@gmail.com>
2020-05-01 13:45:29 -0300

    depoly application, adjust load balancer and deployment

--- scripts/create_cluster.sh
+++ scripts/create_cluster.sh
@@ -1,6 +1,6 @@
 gcloud container clusters create capstone-eks \
   --num-nodes=1 \
-  --cluster-version=1.14.10-gke.27 \
+  --cluster-version=1.15.11-gke.9 \
   --image-type=COS \
   --labels=project=udacity \
   --region=us-central1
