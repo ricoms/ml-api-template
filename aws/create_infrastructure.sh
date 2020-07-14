455778ca4c8b808eda5a0b5620c2ae12edff07a5
Ricardo Savii <ricardosavii@gmail.com>
2020-07-14 07:47:49 -0300

    remove aws ununsed folder

--- aws/create_infrastructure.sh
+++
@@ -1,1 +0,0 @@
-aws cloudformation create-stack --stack-name udacity-capstone-infra --template-body file://infrastructure.yml --parameters file://infrastructure_parameters.json
--- aws/create_worker_nodes.sh
+++
@@ -1,1 +0,0 @@
-aws cloudformation create-stack --stack-name udacity-capstone-nodes --template-body file://worker_nodes.yml --parameters file://worker_nodes_parameters.json --capabilities CAPABILITY_IAM
